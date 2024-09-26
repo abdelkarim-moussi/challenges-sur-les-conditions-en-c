1c2c49a2ccc4fd18c715e10392b8b3ef302b183c
ABDELHAFIDAIT <abdelkarimmoussi73@gmail.com>
2024-09-26 14:59:50 +0100

    challenge13 has been updated

--- challenge13.c
+++ challenge13.c
@@ -6,15 +6,20 @@
 d'un segment, et lit ensuite les coordonnées d'un point dans le plan
 et dit si ce dernier se trouve ou non sur le segment.*/
 int main(){
+    //initialisation des variable
     int x1,x2,x3,y1,y2,y3,a,b;
+    //demande de saisir les coordonnees des extremite
     printf("saisi les coordonnees x et y du premiere extremite :\n");
     scanf("%d %d",&x1,&y1);
     printf("saisi les coordonnees x et y du deuxieme extremite :\n");
     scanf("%d %d",&x2,&y2);
+    //calcule de la pente et b
     a = (y2 - y1)/(x2 - x1);
     b = y1 - a * x1;
+    //demande de saisir les coordonnees de point a verifier
     printf("entrer les coordonnee de votre point :\n");
     scanf("%d %d",&x3,&y3);
+    
     if (y3 == a*x3 + b) printf("le point avec les coordonnees x = %d et y = %d apartien au segment AB",x3,y3);
     else printf("le point avec les coordonnees x = %d et y = %d n'apartien pas au segment AB",x3,y3);
     
